50a5003c0cf7a2e833c7a8e5d61c4a961b1f556e
Thirulogeswaren <vthirulo@outlook.com>
2026-04-20 05:29:41 +0530

    always run cmake cmd

--- cmake/copy-resources.cmake
+++ cmake/copy-resources.cmake
@@ -1,6 +1,13 @@
 
-add_custom_command(TARGET ${CMAKE_PROJECT_NAME} PRE_BUILD
+add_custom_target(shader_files ALL
 	COMMAND ${CMAKE_COMMAND} -E copy_directory_if_different ${CMAKE_SOURCE_DIR}/resources
 	${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${CMAKE_BUILD_TYPE}/resources
-	COMMENT "Copying Shaders to the path ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${CMAKE_BUILD_TYPE}"
+	COMMENT "Copying Shaders to the path ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${CMAKE_BUILD_TYPE}" VERBATIM
 )
+
+add_custom_command(TARGET ${CMAKE_PROJECT_NAME} POST_BUILD VERBATIM
+	COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_BINARY_DIR}/compile_commands.json
+	${CMAKE_SOURCE_DIR}/compile_commands.json 
+)
+
+add_dependencies(shader_files ${CMAKE_PROJECT_NAME})
